417de6546b816803d745aedfb3917598e0a99ccd
dpdp5592 <2232728111@qq.com>
2025-11-17 22:45:37 +0800

    Add files via upload

--- diagnose.py
+++ diagnose.py
@@ -42,5 +42,16 @@ def simple_diagnose(memory: EpisodicMemory, key: str):
 
 if __name__ == "__main__":
     mem = EpisodicMemory()
-    # 先不加任何记忆，故意触发“缺失记忆”的报告
+
+    print("【第一次年检】什么记忆都没有：")
     simple_diagnose(mem, "demo_traj")
+
+    print("\n现在往记忆里加一条记录……\n")
+    # 给机器人加一条“demo_traj”的记忆
+    fake_traj = [0, 1, 2, 3]       # 假装是机器人的轨迹
+    fake_sensors = {"force": 0.1}  # 假装是传感器信息
+    mem.add("demo_traj", fake_traj, fake_sensors)
+
+    print("【第二次年检】已经有记忆了：")
+    simple_diagnose(mem, "demo_traj")
+
